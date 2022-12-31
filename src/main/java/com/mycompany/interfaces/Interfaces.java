4cc503b44e39f6e405b0863aa2ddb9fbab604ab1
houss <houssam2001mchmch@gmail.com>
2022-12-31 13:17:44 +0100

    premiere

---
+++ src/main/java/com/mycompany/interfaces/Interfaces.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.interfaces;
+
+/**
+ *
+ * @author houss
+ */
+public class Interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
